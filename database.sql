2aa8ea3e4911ad81e0269c387b0c78e20ee1c554
Ashley Berry <ashleywberry@gmail.com>
2020-10-01 14:20:36 -0500

    added favorites table and default input

--- database.sql
+++ database.sql
@@ -12,3 +12,17 @@ CREATE TABLE "category" (
 -- Default categories. You may change them :)
 INSERT INTO "category" ("name")
 VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
+
+--Favorites table
+CREATE TABLE "favorites" (
+    "id" SERIAL PRIMARY KEY,
+    "url" VARCHAR (100) NOT NULL,
+    "category_id" INTEGER NOT NULL
+);
+
+INSERT INTO "favorites" ("url", "category_id")
+VALUES ('https://giphy.com/gifs/this-is-fine-QMHoU66sBXqqLqYvGO', '5'), 
+('https://giphy.com/gifs/trash-dumpster-fire-Jrl4FlTaymFFbNiwU5', '3'), 
+('https://giphy.com/gifs/smile-nicolas-cage-nic-PHHtPDk6peKyI', '2'), 
+('https://giphy.com/gifs/disney-sloth-sloths-zootopia-IG6i30q4DMT0Q', '3'), 
+('https://giphy.com/gifs/moodman-cat-2020-wack-kefZA9vr3rKCJThLjL', '1');
