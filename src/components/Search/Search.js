c72742382b0eb863df40931dd5abd23df7fa1433
Jake Stucky <jakestucky@gmnail.com>
2020-10-01 16:20:11 -0500

    get router created and workign to bring down 10 cat gifs

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Search extends Component {
+  state = {
+    giphyData: [],
+  };
 
   componentDidMount = () => {
-    this.getGif()
-  }
+    axios({
+      method: 'GET',
+      url: '/api/search',
+    })
+      .then((res) => {
+        console.log('res is', res);
 
-  getGif = () => {
-    console.log( 'in getGIF' )
-    // this.props.dispatch({
-    //   type: 'FETCH_GIF' ???
-    // })
-  }
+        this.setState({
+          giphyData: res.data.data,
+        });
+      })
+      .catch((err) => {
+        console.log('error on client side');
+
+        console.error(err);
+      });
+  };
 
   render() {
     return (
       <div>
         <h1>We're in Search!</h1>
-        <input required type= "text" placeholder="Search for GIF"></input>
-        <button onClick={ this.getGif }>Submit</button>
+
+        <input required type='text' placeholder='Search for GIF'></input>
+        <button onClick={this.getNewGif}>Submit</button>
+        <br></br>
+        {this.state.giphyData.map((item) => (
+          <img src={item.images.fixed_width_small.url} />
+        ))}
       </div>
     );
   }
-  
 }
-
-export default Search;
+const mapStateToProps = (reduxState) => ({
+  reduxState,
+});
+export default connect(mapStateToProps)(Search);
--- src/index.js
+++ src/index.js
@@ -2,44 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import { Provider } from 'react-redux';
-import logger from "redux-logger";
+import logger from 'redux-logger';
 //import registerServiceWorker from "./registerServiceWorker";
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-// import axios from "axios";
+import axios from 'axios';
 
 // Import saga middleware
-import createSagaMiddleware from "redux-saga";
-import { put, takeEvery } from "redux-saga/effects"; 
+import createSagaMiddleware from 'redux-saga';
+import { put, takeEvery } from 'redux-saga/effects';
 
 const gifReducer = (state = [], action) => {
-    //PLACEHOLDER STATEMENTS
-    if ( action.type === "SET_GIFS" ) {
-        return action.type
-    }
-    return state
-}
+  //PLACEHOLDER STATEMENTS
+  if (action.type === 'SET_GIFS') {
+    return action.payload;
+  }
+  return state;
+};
 
-function* placeHolder( action ) {
-    console.log( 'in saga placeHolder:', action )
+function* placeHolder(action) {
+  console.log('in saga placeHolder:', action);
 }
 
 // main saga function
 function* rootSaga() {
-    yield takeEvery( 'SET_GIFS', placeHolder );
-    //takeEvery go here
+  yield takeEvery('SET_GIFS', placeHolder);
+  //   yield takeEvery('GET_GIFS', getGiphyApi);
+  //takeEvery go here
 }
 
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-    combineReducers({ gifReducer }),
-    //Add sagaMiddleware to our store
-    applyMiddleware( sagaMiddleware, logger )
-  );
-  
+  combineReducers({ gifReducer }),
+  //Add sagaMiddleware to our store
+  applyMiddleware(sagaMiddleware, logger)
+);
+
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('react-root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('react-root')
+);
 //registerServiceWorker();
