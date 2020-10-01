5b62dafea752cb22c558372d9618745104579b7b
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-10-01 14:22:09 -0500

    set up reduxstate

--- src/index.js
+++ src/index.js
@@ -1,45 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+import { Provider } from "react-redux";
 import logger from "redux-logger";
 //import registerServiceWorker from "./registerServiceWorker";
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from "redux";
 // import axios from "axios";
 
 // Import saga middleware
 import createSagaMiddleware from "redux-saga";
-import { put, takeEvery } from "redux-saga/effects"; 
+import { put, takeEvery } from "redux-saga/effects";
+
+// const initialState = {
+// 	funny: "",
+// 	cohort: "",
+// 	cartoon: "",
+// 	nsfw: "",
+// 	meme: "",
+// };
 
 const gifReducer = (state = [], action) => {
-    //PLACEHOLDER STATEMENTS
-    if ( action.type === "SET_GIFS" ) {
-        return action.type
-    }
-    return state
-}
+	//PLACEHOLDER STATEMENTS
+	if (action.type === "SET_GIFS") {
+		return action.type;
+	}
+	// if (action.type === "CLEAR_VALUES") {
+	// 	console.log(initialState, state);
+	// 	return initialState;
+	// }
+	return state;
+};
 
-function* placeHolder( action ) {
-    console.log( 'in saga placeHolder:', action )
+function* placeHolder(action) {
+	console.log("in saga placeHolder:", action);
 }
 
 // main saga function
 function* rootSaga() {
-    yield takeEvery( 'SET_GIFS', placeHolder );
-    //takeEvery go here
+	yield takeEvery("SET_GIFS", placeHolder);
+	//takeEvery go here
 }
 
 // Create sagaMiddleware
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-    combineReducers({ gifReducer }),
-    //Add sagaMiddleware to our store
-    applyMiddleware( sagaMiddleware, logger )
-  );
-  
+	combineReducers({ gifReducer }),
+	//Add sagaMiddleware to our store
+	applyMiddleware(sagaMiddleware, logger)
+);
+
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('react-root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("react-root")
+);
 //registerServiceWorker();
